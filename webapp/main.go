a572cb34b494991464e5dfe476bc900d69f93636
YURII D. <gkhelloworld@gmail.com>
2024-10-12 12:34:40 +0300

    update i18n

--- webapp/main.go
+++ webapp/main.go
@@ -4,61 +4,53 @@ import (
 	"log"
 
 	"github.com/BurntSushi/toml"
-	"github.com/gofiber/contrib/fiberi18n/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/pug/v2"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 )
 
-func localizeOrDefault(c *fiber.Ctx, key, defaultValue string) string {
-	localized, err := fiberi18n.Localize(c, key)
-	if err != nil {
-		return defaultValue
-	}
-	return localized
-}
-
 func main() {
-	engine := pug.New("./webapp/views", ".pug")
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+	bundle.MustLoadMessageFile("localize/active.uk.toml")
+
+	engine := pug.New("./views", ".pug")
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
-	i18nMiddleware := fiberi18n.New(&fiberi18n.Config{
-		RootPath:         "./webapp/localize",
-		AcceptLanguages:  []language.Tag{language.Ukrainian, language.English},
-		DefaultLanguage:  language.English,
-		FormatBundleFile: "toml",
-		UnmarshalFunc:    toml.Unmarshal,
-	})
-
-	app.Use(i18nMiddleware)
-	app.Use(func(c *fiber.Ctx) error {
-		t_darkModeSwitch := localizeOrDefault(c, "darkModeSwitch", "Dark Mode")
-		log.Println(t_darkModeSwitch)
-		c.Locals("t_darkMode", t_darkModeSwitch)
-		return c.Next()
-	})
-
 	app.Get("/", func(c *fiber.Ctx) error {
-		lang := c.Query("lang", "en")
 
-		return c.Render("index", fiber.Map{
+		lang := c.Query("lang", "en")
+		return c.Render("main/index", fiber.Map{
 			"Title": "Test",
 			"lang":  lang,
 		})
 	})
 
 	app.Get("/test", func(c *fiber.Ctx) error {
-		localize, err := fiberi18n.Localize(c, "hello")
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-		return c.SendString(localize)
+		lang := c.Query("lang", "en")
+		accept := c.AcceptsLanguages()
+		localizer := i18n.NewLocalizer(bundle, lang, accept)
+
+		helloPerson := localizer.MustLocalize(&i18n.LocalizeConfig{
+			DefaultMessage: &i18n.Message{
+				ID:    "HelloPerson",
+				Other: "Hello {{.Name}}",
+			},
+			TemplateData: map[string]string{
+				"Name": "YURII",
+			},
+		})
+
+		return c.Render("exchange/index", fiber.Map{
+			"Title": helloPerson,
+		})
 	})
 
-	app.Static("/static", "./webapp/static")
+	app.Static("/static", "./static")
 
 	log.Fatal(app.Listen(":3000"))
 }
