9b42d54cdc23deb0c2ea126fd6dd6e3ea6acd9d2
YURII D. <gkhelloworld@gmail.com>
2024-10-16 11:02:34 +0300

    up

--- webapp/handlers/home.go
+++ webapp/handlers/home.go
@@ -7,5 +7,10 @@ import (
 func Home(c *fiber.Ctx) error {
 	return c.Render("main/index", fiber.Map{
 		"Title": "Welcome to the Currency App",
+		"Numbers": map[int]string{
+			1: "one",
+			2: "two",
+			3: "three",
+		},
 	})
 }
